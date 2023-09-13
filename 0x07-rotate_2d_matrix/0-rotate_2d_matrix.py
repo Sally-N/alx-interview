b86046b76179198c2d371cb039d9b61fec31069a
Sally-N <snzungula@gmail.com>
2023-09-13 17:51:01 +0000

    rotate a 2D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Rotates a 2D matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+       Rotates 2D matrix 90 degrees clockwise
+       Matrix is edited in-place
+       args:
+          matrix
+    """
+    #transpose
+    for i in range(len(matrix)):
+        for j in range(i,len(matrix)):
+            matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+
+
+    #reverse
+    N=len(matrix)
+    for i in range(N//2):
+        for j in range(N):
+            matrix[j][i],matrix[j][N-1-i]=matrix[j][N-1-i],matrix[j][i]
